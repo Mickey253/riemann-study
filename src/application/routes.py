4d2e4536690fabad15e2e1563373d0b62bf08d49
Dhruv Bhatia <thedhruv26@gmail.com>
2023-10-10 18:14:11 -0700

    routing for different visualization basedd on user ID

--- src/application/routes.py
+++ src/application/routes.py
@@ -1,6 +1,10 @@
-from flask import jsonify, render_template, request
+from flask import jsonify, render_template, request, redirect, url_for
 from application import app
 from werkzeug.utils import secure_filename
+
+euc_users = ["E1", "E2", "E3"]
+sph_users = ["S1", "S2", "S3"]
+hyp_users = ["H1", "H2", "H3"]
 
 @app.route('/')
 @app.route('/index')
@@ -10,5 +14,49 @@ def index():
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title='template')
+    return render_template('about.html', title='About')
+
+@app.route('/euclidean/homepage') 
+def euc_view():
+    id = request.args.get('id')
+    if id in euc_users:
+        return render_template("visualization.html", title='Euclidean', data=None, id=id)
+    return redirect(url_for("index"))
+
+@app.route('/spherical/homepage') 
+def sph_view():
+    id = request.args.get('id')
+    if id in sph_users:
+        return render_template("visualization.html", title='Spherical', data=None, id=id)
+    return redirect(url_for("index"))
+
+@app.route('/hyperbolic/homepage') 
+def hyp_view():
+    id = request.args.get('id')
+    if id in hyp_users:
+        return render_template("visualization.html", title='Hyperbolic', data=None, id=id)
+    return redirect(url_for("index"))
+
+@app.route('/index/<id>')
+def user_index(id):
+    if id in euc_users:
+        return redirect(url_for("euc_view", id=id))
+    elif id in sph_users:
+        return redirect(url_for("sph_view", id=id))
+    elif id in hyp_users:
+        return redirect(url_for("hyp_view", id=id))
+    else:
+        return render_template('errors/404.html'), 404
+
+@app.route('/index/')
+def user_index_form():
+    id = request.args.get('id')
+    if id in euc_users:
+        return redirect(url_for("euc_view", id=id))
+    elif id in sph_users:
+        return redirect(url_for("sph_view", id=id))
+    elif id in hyp_users:
+        return redirect(url_for("hyp_view", id=id))
+    else:
+        return render_template('errors/404.html'), 404
 
