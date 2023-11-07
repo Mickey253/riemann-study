eb27fe98d29dacb5b21cddc357afe14d40d534ad
Dhruv Bhatia <thedhruv26@gmail.com>
2023-11-07 00:24:38 -0700

    added homepage before test

--- src/application/routes.py
+++ src/application/routes.py
@@ -18,26 +18,44 @@ def about():
     return render_template('about.html', title='About')
 
 @app.route('/euclidean/homepage') 
-def euc_view():
+def euc_view_home():
     id = request.args.get('id')
+    if id in euc_users:
+        return render_template("euc-vis-home.html", title='Euclidean Homepage', data=None, id=id)
+    return redirect(url_for("index"))
+
+@app.route('/spherical/homepage') 
+def sph_view_home():
+    id = request.args.get('id')
+    if id in sph_users:
+        return render_template("sph-vis-home.html", title='Spherical', data=None, id=id, q_id="N/A")
+    return redirect(url_for("index"))
+
+@app.route('/hyperbolic/homepage') 
+def hyp_view_home():
+    id = request.args.get('id')
+    if id in hyp_users:
+        return render_template("hyp-vis-home.html", title='Hyperbolic', data=None, id=id, q_id="N/A")
+    return redirect(url_for("index"))
+
+@app.route('/euclidean/test<id>') 
+def euc_view(id):
     if id in euc_users:
         with open("src/application/data/test-graph-0.json", 'r') as fdata:
             gdata = json.load(fdata)
         return render_template("visualization.html", title='Euclidean', data=gdata, id=id, q_id="N/A")
     return redirect(url_for("index"))
 
-@app.route('/spherical/homepage') 
-def sph_view():
-    id = request.args.get('id')
+@app.route('/spherical/test<id>') 
+def sph_view(id):
     if id in sph_users:
         with open("src/application/data/sphere-test.json", 'r') as fdata:
             gdata = json.load(fdata)
         return render_template("sphere-visualization.html", title='Spherical', data=gdata, id=id, q_id="N/A")
     return redirect(url_for("index"))
 
-@app.route('/hyperbolic/homepage') 
-def hyp_view():
-    id = request.args.get('id')
+@app.route('/hyperbolic/test<id>') 
+def hyp_view(id):
     if id in hyp_users:
         return render_template("visualization.html", title='Hyperbolic', data=None, id=id, q_id="N/A")
     return redirect(url_for("index"))
@@ -45,11 +63,11 @@ def hyp_view():
 @app.route('/index/<id>')
 def user_index(id):
     if id in euc_users:
-        return redirect(url_for("euc_view", id=id))
+        return redirect(url_for("euc_view_home", id=id))
     elif id in sph_users:
-        return redirect(url_for("sph_view", id=id))
+        return redirect(url_for("sph_view_home", id=id))
     elif id in hyp_users:
-        return redirect(url_for("hyp_view", id=id))
+        return redirect(url_for("hyp_view_home", id=id))
     else:
         return render_template('errors/404.html'), 404
 
@@ -57,11 +75,11 @@ def user_index(id):
 def user_index_form():
     id = request.args.get('id')
     if id in euc_users:
-        return redirect(url_for("euc_view", id=id))
+        return redirect(url_for("euc_view_home", id=id))
     elif id in sph_users:
-        return redirect(url_for("sph_view", id=id))
+        return redirect(url_for("sph_view_home", id=id))
     elif id in hyp_users:
-        return redirect(url_for("hyp_view", id=id))
+        return redirect(url_for("hyp_view_home", id=id))
     else:
         return render_template('errors/404.html'), 404
 
